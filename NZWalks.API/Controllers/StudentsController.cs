e070f0c42a812be9d692a8c25918e7cc44957d39
Maitry <maitrithaker1239@gmail.com>
2024-07-31 11:26:10 +0530

    create models

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:portnumber/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //Get:https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "john", "jane", "mark", "Emily", "Rick", "Devid" };
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walks.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walks
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string WalkImageUrl { get; set; }
+        public Guid DifficultyID { get; set; }
+        public Guid RegionID { get; set; }
+
+        //Nevigation Property
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+
+    }
+}
