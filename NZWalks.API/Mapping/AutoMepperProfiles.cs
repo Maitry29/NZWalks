465bd2184f593ce8261db146989703ceb2839442
Maitry <maitrithaker1239@gmail.com>
2024-08-04 10:16:33 +0530

    SAVE#

---
+++ NZWalks.API/Mapping/AutoMepperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Mapping
+{
+    public class AutoMepperProfiles : Profile
+    {
+        public AutoMepperProfiles()
+        {
+            CreateMap<Region,RegionDTO>().ReverseMap();
+            CreateMap<AddRegionRequestDTO,Region>().ReverseMap();
+            CreateMap<UpdateRegionRequestDTO, Region>().ReverseMap();
+
+        }
+    }
+}
